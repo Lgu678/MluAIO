64f3baa6e458a19cf9d8a65cac0b64564ccc36c1
lgu <laggybaggy5162@gmail.com>
2023-07-12 02:07:13 -0400

    changes

--- PluginTemplate/SimplePlugin/taric.h
+++ PluginTemplate/SimplePlugin/taric.h
@@ -3,4 +3,7 @@ namespace taric
 {
     void load();
     void unload();
+    void add_circle(vector const& center, float radius, unsigned long color, float thickness = 1.f, float height = -1.f, int num_segments = 200);
+    void drawCircle(vector pos, int radius, int quality, unsigned long color, int thickness); // New function declaration
+    void on_draw_with_add_circle();
 };
